8031ba3d821f8cc5c7fb2ad11e08fedf69ba63fb
9sw <89306314+midniqhtowls@users.noreply.github.com>
2022-06-20 18:37:30 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Imports
+
+import discord
+import asyncio
+import random
+import datetime
+import time
+from discord import user
+from discord import message
+from discord.activity import Game
+from discord.user import User
+from discord.ext import commands
+from discord import guild
+from discord.ext import tasks
+from discord import mentions
+from discord.ext.commands import Bot, has_permissions
+from discord import Embed
+from discord.utils import get
+
+version = "1.0.0"
+username = "bot"
+prefix = "!"
+token = "token"
