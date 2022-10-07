a3e885873257726092e0f5d561471511d6578fec
9sw <89306314+midniqhtowls@users.noreply.github.com>
2022-10-07 11:50:47 +0700

    Update 6 [v1.0.6]
    [+] Added delete_ownMessage [in development]
    [+] Added statusType [in development]
    [+] Added onlineStatus [in development]
    [/] Fixed status not showing
    [/] Fixed delete_after not working on some commands

--- main.py
+++ main.py
@@ -19,14 +19,20 @@ from discord.utils import get
 
 # Variables 
 
-version = "1.0.0"
-prefix = "!"
-username = "username"
-status = "status"
-token = "token"
+version = "1.0.6" # not really important, just the footer of the embed and keeps track of the updates on the github, you'll have to keep checking the github for updates/bug fixes
+prefix = "!" # sets the prefix, you can make it anything you want
+username = "username" # for embed only, you will have to set the actual bot username in your discord developer application
+token = "token" # bots token, needed to run the bot
+status = "status" # sets the status, you can also make thing anything you want [inDev]
+statusType = "watching" # watching, playing, listening, streaming [inDev]
+onlineStatus = "online" # online, idle, do_not_disturb, offline [inDev]
 delete_after = True # set this to false if you dont want the bot to delete its own message after 20 seconds
+delete_ownMessage = True # set this to false if you dont want the bot to delete your own message after running the command
 
-bot = commands.Bot(command_prefix=f"{prefix}", status=discord.Status.idle,activity=discord.Activity(type=discord.ActivityType.watching, name=f"{status}" , case_insensitive=True, intents=discord.Intents.all()))
+# DO NOT CHANGE ANYTHING UNDER HERE IF YO DO NOT KNOW WHAT YOU ARE DOING!
+
+intents = discord.Intents(messages=True, guilds=True)
+bot = commands.Bot(command_prefix=f"{prefix}", status=discord.Status.idle,activity=discord.Activity(type=discord.ActivityType.watching, name=f"{status}" , case_insensitive=True, intents=intents))
 bot.remove_command('help')
 
 # Start up
@@ -47,9 +53,10 @@ async def on_command_error(ctx, error):
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
         embed.add_field(name="**failed**",value="command not found", inline=False)
         embed.set_footer(text=f"{username} is currently at {version}")
-        await ctx.message.delete()
-        if delete_after == True:
+        if delete_after and delete_ownMessage == True:
             await ctx.send(embed=embed, delete_after=20.0)
+            await ctx.message.delete()
+            await ctx.send(embed=embed)
         else:
             await ctx.send(embed=embed)
 
@@ -63,9 +70,10 @@ async def help(ctx):
     embed.add_field(name=f"{prefix}membercount",value="shows the server's member count", inline=False)
     embed.add_field(name=f"{prefix}embed",value="sends a embed with a title and a description", inline=False)
     embed.set_footer(text=f"{username} is currently at {version}")
-    await ctx.message.delete()
-    if delete_after == True:
+    if delete_after and delete_ownMessage == True:
         await ctx.send(embed=embed, delete_after=20.0)
+        await ctx.message.delete()
+        await ctx.send(embed=embed)
     else:
         await ctx.send(embed=embed)
 
@@ -76,9 +84,10 @@ async def membercount(ctx):
     embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
     embed.add_field(name=f"**membercount**",value=f"`{guild.member_count}` members", inline=False)
     embed.set_footer(text=f"{username} is currently at {version}")
-    await ctx.message.delete()
-    if delete_after == True:
+    if delete_after and delete_ownMessage == True:
         await ctx.send(embed=embed, delete_after=20.0)
+        await ctx.message.delete()
+        await ctx.send(embed=embed)
     else:
         await ctx.send(embed=embed)
 
@@ -87,9 +96,10 @@ async def embed(ctx, content, reason):
         embed = discord.Embed(color=0x2F3136)
         embed.add_field(name=content,value=reason)
         embed.set_footer(text=f"{username} is currently at {version}")
-        await ctx.message.delete()
-        if delete_after == True:
+        if delete_after and delete_ownMessage == True:
             await ctx.send(embed=embed, delete_after=20.0)
+            await ctx.message.delete()
+            await ctx.send(embed=embed)
         else:
             await ctx.send(embed=embed)
 
